bdde555503871bcf0a2ff404caedfc9c664c672e
Pavel Sakhanko <iospaveldev@gmail.com>
2020-11-08 13:13:28 +0300

    feat: added custom ViewModifier

--- challenge_24Day/ContentView.swift
+++ challenge_24Day/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .titleStyle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct LargeBlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.blue)
+            .font(.system(size: 30))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(LargeBlueTitle())
     }
 }
