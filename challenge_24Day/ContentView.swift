24a93504b5df32173675a23efd0d2cbe52076594
Pavel Sakhanko <iospaveldev@gmail.com>
2020-11-08 12:56:17 +0300

    Initial Commit

---
+++ challenge_24Day/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge_24Day
+//
+//  Created by Pavel Sakhanko on 8.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge_24Day/challenge_24DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_24DayApp.swift
+//  challenge_24Day
+//
+//  Created by Pavel Sakhanko on 8.11.20.
+//
+
+import SwiftUI
+
+@main
+struct challenge_24DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
